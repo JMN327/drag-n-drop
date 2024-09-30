0c8e0c799af544b29a7d239a6a3a3fe6a128526d
jmn327 <173676271+JMN327@users.noreply.github.com>
2024-09-30 18:43:46 +0100

    More work on functionality

--- dragDrop.js
+++ dragDrop.js
@@ -1,24 +1,25 @@
 const gridContainer = document.querySelector(".grid-container");
 gridContainerStyles = getComputedStyle(gridContainer);
 let gap = parseInt(gridContainerStyles.getPropertyValue("gap"));
+let padding = parseInt(gridContainerStyles.getPropertyValue("padding"));
 
 let row = null;
 let elder = null;
-let elderY = null
+let elderY = null;
 let younger = null;
-let youngerY = null
+let youngerY = null;
 let shiftY = null;
 let startY = null;
 let currentYPos = null;
+let maxPos = null;
+let minPos = null;
 let switchOffset = 0;
-
-gridContainer.addEventListener("dragstart", (event) => {
-  event.preventDefault();
-  return false;
-});
 
 gridContainer.addEventListener("mousedown", (event) => {
   if (!event.target.classList.contains("grid-item")) {
+    return;
+  }
+  if (event.button !== 0) {
     return;
   }
   row = event.target;
@@ -38,33 +39,64 @@ gridContainer.addEventListener("mousemove", (event) => {
 
   if (elder) {
     let switchCheck = row.getBoundingClientRect().top;
-    if (switchCheck < elderY) {
-      switchOffset += gap + elder.offsetHeight;
+    if (switchCheck <= elderY) {
+      let currentHeight = elder.offsetHeight;
+      switchOffset += gap + currentHeight;
       row.parentNode.insertBefore(row, elder);
       getImmediateSiblings(row);
+      const snap = [
+        { transform: `translate(0px, ${-currentHeight}px)` },
+        { transform: `translate(0px, 0px)` },
+      ];
+      const snapTiming = {
+        duration: 100,
+        iterations: 1,
+      };
+
+      younger.animate(snap, snapTiming);
     }
   }
 
   if (younger) {
     let switchCheck = row.getBoundingClientRect().top;
-    if (switchCheck > youngerY) {
-      switchOffset -= gap + younger.offsetHeight;
+    if (switchCheck >= youngerY) {
+      let currentHeight = younger.offsetHeight;
+      switchOffset -= gap + currentHeight;
       row.parentNode.insertBefore(younger, row);
       getImmediateSiblings(row);
+      const snap = [
+        { transform: `translate(0px, ${currentHeight}px)` },
+        { transform: `translate(0px, 0px)` },
+      ];
+      const snapTiming = {
+        duration: 100,
+        iterations: 1,
+      };
+
+      elder.animate(snap, snapTiming);
     }
   }
-  
 
   let mouseYRelToContainer = event.clientY - startY;
   currentYPos = mouseYRelToContainer + switchOffset - shiftY;
+  relativePos =
+    event.clientY - shiftY - gridContainer.getBoundingClientRect().top - 2;
+
   row.style.top = currentYPos + "px";
+/*   if (currentYPos < -11) {
+    row.style.top = 0;
+  } */
+
+  console.log(relativePos, currentYPos, row.style.top);
 });
 
 gridContainer.addEventListener("mouseup", (event) => {
-  let endYPos = currentYPos;
-  const snap = [{ transform: `translate(0px, ${-endYPos}px)` }];
+  if (!row) {
+    return;
+  }
+  const snap = [{ transform: `translate(0px, ${-currentYPos}px)` }];
   const snapTiming = {
-    duration: 200,
+    duration: 100,
     iterations: 1,
   };
 
@@ -77,6 +109,7 @@ gridContainer.addEventListener("mouseup", (event) => {
     shiftY = null;
     startY = null;
     switchOffset = 0;
+    currentYPos = 0;
   };
 });
 
