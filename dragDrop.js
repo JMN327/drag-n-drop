8f3e28b193cef4687a01fac38f5f8a8c754c1536
JMN327 <173676271+JMN327@users.noreply.github.com>
2024-10-01 16:46:52 +0100

    Added functionality for multiple instances to work simultaneously

--- dragDrop.js
+++ dragDrop.js
@@ -1,158 +1,161 @@
-const gridContainer = document.querySelector(".grid-container.ape");
-const gridContainerStyles = getComputedStyle(gridContainer);
-let gap = parseInt(gridContainerStyles.getPropertyValue("gap"));
-let padding = parseInt(gridContainerStyles.getPropertyValue("padding"));
+const gridContainerList = document.querySelectorAll(".grid-container");
+const gridContainerArray = [...gridContainerList];
 
-let item = null;
-let itemAbove = null;
-let itemAboveY = null;
-let itemBelow = null;
-let itemBelowY = null;
-let gridContainerTop = null;
-let pointerOffset = null;
-let initialContainerPosY = null;
-let itemLocalPosY = null;
-let switchOffset = 0;
-let animating = false;
+gridContainerArray.forEach((gridContainer) => {
+  makeDragNDrop(gridContainer);
+});
 
-let sticky = false;
+function makeDragNDrop(gridContainer) {
+  const gridContainerStyles = getComputedStyle(gridContainer);
+  let gap = parseInt(gridContainerStyles.getPropertyValue("gap"));
+  let padding = parseInt(gridContainerStyles.getPropertyValue("padding"));
 
-gridContainer.addEventListener("mousedown", (event) => pickUpGridItem(event));
-gridContainer.addEventListener("mousemove", (event) => moveGridItem(event));
-gridContainer.addEventListener("mouseup", (event) => releaseGridItem(event));
+  let item = null;
+  let itemAbove = null;
+  let itemAboveY = null;
+  let itemBelow = null;
+  let itemBelowY = null;
+  let gridContainerTop = null;
+  let pointerOffset = null;
+  let initialContainerPosY = null;
+  let itemLocalPosY = null;
+  let switchOffset = 0;
+  let animating = false;
 
-if (sticky) {
-  gridContainer.addEventListener("mouseenter", (event) => {
-    if (event.buttons !== 1) {
-      releaseGridItem(event);
-    }
-  });
-} else {
-  gridContainer.addEventListener("mouseleave", (event) => {
-    if (event.buttons == 1) {
-      releaseGridItem(event);
-    }
-  });
-}
+  let sticky = false;
 
-function pickUpGridItem(event) {
-  if (!event.target.classList.contains("grid-item")) {
-    return;
-  }
-  if (event.buttons !== 1) {
-    return;
-  }
-  if (animating) {
-    return;
-  }
-  item = event.target;
-  item.classList.add("grabbed");
-  item.style.zIndex = 1000;
+  gridContainer.addEventListener("mousedown", (event) => pickUpGridItem(event));
+  gridContainer.addEventListener("mousemove", (event) => moveGridItem(event));
+  gridContainer.addEventListener("mouseup", (event) => releaseGridItem(event));
 
-  getImmediateSiblings(item);
-
-  gridContainerTop = gridContainer.getBoundingClientRect().top
-  pointerOffset = event.offsetY;
-  initialContainerPosY = item.getBoundingClientRect().top;
-}
-
-function moveGridItem(event) {
-  if (!item) {
-    return;
-  }
-  if (animating) {
-    return;
-  }
-
-  itemContainerPosY =
-    event.clientY - pointerOffset - gridContainerTop;
-
-  console.log(itemContainerPosY);
-
-  if (itemAbove) {
-    if (itemContainerPosY <= itemAboveY) {
-      let itemHeightSnapshot = itemAbove.offsetHeight;
-      switchOffset += gap + itemHeightSnapshot;
-      item.parentNode.insertBefore(item, itemAbove);
-      getImmediateSiblings(item);
-
-      animateSnap(itemBelow, -itemHeightSnapshot, 0, 150);
-    }
-  }
-
-  if (itemBelow) {
-    if (itemContainerPosY >= itemBelowY) {
-      let itemHeightSnapshot = itemBelow.offsetHeight;
-      switchOffset -= gap + itemHeightSnapshot;
-      item.parentNode.insertBefore(itemBelow, item);
-      getImmediateSiblings(item);
-
-      animateSnap(itemAbove, itemHeightSnapshot, 0, 150);
-    }
-  }
-
-  itemLocalPosY =
-    event.clientY - initialContainerPosY + switchOffset - pointerOffset;
-
-  if (itemContainerPosY < 0) {
-    item.parentNode.prepend(item);
-    itemLocalPosY = -padding;
-    item.style.top = itemLocalPosY + "px";
-  } else if (
-    itemContainerPosY >
-    gridContainer.offsetHeight - item.offsetHeight
-  ) {
-    item.parentNode.append(item);
-    itemLocalPosY = padding;
-    item.style.top = itemLocalPosY + "px";
+  if (sticky) {
+    gridContainer.addEventListener("mouseenter", (event) => {
+      if (event.buttons !== 1) {
+        releaseGridItem(event);
+      }
+    });
   } else {
-    item.style.top = itemLocalPosY + "px";
+    gridContainer.addEventListener("mouseleave", (event) => {
+      if (event.buttons == 1) {
+        releaseGridItem(event);
+      }
+    });
   }
 
-  getImmediateSiblings(item);
-}
+  function pickUpGridItem(event) {
+    if (!event.target.classList.contains("grid-item")) {
+      return;
+    }
+    if (event.buttons !== 1) {
+      return;
+    }
+    if (animating) {
+      return;
+    }
+    item = event.target;
+    item.classList.add("grabbed");
+    item.style.zIndex = 1000;
 
-function releaseGridItem(event) {
-  if (!item) {
-    return;
+    getImmediateSiblings(item);
+
+    gridContainerTop = gridContainer.getBoundingClientRect().top;
+    pointerOffset = event.offsetY;
+    initialContainerPosY = item.getBoundingClientRect().top;
   }
-  const snapAnimation = animateSnap(item, 0, -itemLocalPosY, 150);
-  snapAnimation.onfinish = () => {
-    item.style.top = 0 + "px";
-    item.style.zIndex = 0;
-    item.classList.remove("grabbed");
-    item = null;
-    pointerOffset = null;
-    initialContainerPosY = null;
-    switchOffset = 0;
-    itemLocalPosY = 0;
-    animating = false;
-  };
-}
 
-function getImmediateSiblings(currentItem) {
-  itemAbove = currentItem.previousElementSibling;
-  itemBelow = currentItem.nextElementSibling;
-  if (itemAbove) {
-    itemAboveY =
-      itemAbove.getBoundingClientRect().top -
-      gridContainerTop;
+  function moveGridItem(event) {
+    if (!item) {
+      return;
+    }
+    if (animating) {
+      return;
+    }
+
+    itemContainerPosY = event.clientY - pointerOffset - gridContainerTop;
+
+    console.log(itemContainerPosY);
+
+    if (itemAbove) {
+      if (itemContainerPosY <= itemAboveY) {
+        let itemHeightSnapshot = itemAbove.offsetHeight;
+        switchOffset += gap + itemHeightSnapshot;
+        item.parentNode.insertBefore(item, itemAbove);
+        getImmediateSiblings(item);
+
+        animateSnap(itemBelow, -itemHeightSnapshot, 0, 150);
+      }
+    }
+
+    if (itemBelow) {
+      if (itemContainerPosY >= itemBelowY) {
+        let itemHeightSnapshot = itemBelow.offsetHeight;
+        switchOffset -= gap + itemHeightSnapshot;
+        item.parentNode.insertBefore(itemBelow, item);
+        getImmediateSiblings(item);
+
+        animateSnap(itemAbove, itemHeightSnapshot, 0, 150);
+      }
+    }
+
+    itemLocalPosY =
+      event.clientY - initialContainerPosY + switchOffset - pointerOffset;
+
+    if (itemContainerPosY < 0) {
+      item.parentNode.prepend(item);
+      itemLocalPosY = -padding;
+      item.style.top = itemLocalPosY + "px";
+    } else if (
+      itemContainerPosY >
+      gridContainer.offsetHeight - item.offsetHeight
+    ) {
+      item.parentNode.append(item);
+      itemLocalPosY = padding;
+      item.style.top = itemLocalPosY + "px";
+    } else {
+      item.style.top = itemLocalPosY + "px";
+    }
+
+    getImmediateSiblings(item);
   }
-  if (itemBelow) {
-    itemBelowY =
-      itemBelow.getBoundingClientRect().top -
-      gridContainerTop;
+
+  function releaseGridItem(event) {
+    if (!item) {
+      return;
+    }
+    const snapAnimation = animateSnap(item, 0, -itemLocalPosY, 150);
+    snapAnimation.onfinish = () => {
+      item.style.top = 0 + "px";
+      item.style.zIndex = 0;
+      item.classList.remove("grabbed");
+      item = null;
+      pointerOffset = null;
+      initialContainerPosY = null;
+      switchOffset = 0;
+      itemLocalPosY = 0;
+      animating = false;
+    };
   }
-}
 
-function animateSnap(thisItem, startPosition, endPosition, durationMS) {
-  const snap = [
-    { transform: `translate(0px, ${startPosition}px)` },
-    { transform: `translate(0px, ${endPosition}px)` },
-  ];
-  const snapTiming = {
-    duration: durationMS,
-  };
+  function getImmediateSiblings(currentItem) {
+    itemAbove = currentItem.previousElementSibling;
+    itemBelow = currentItem.nextElementSibling;
+    if (itemAbove) {
+      itemAboveY = itemAbove.getBoundingClientRect().top - gridContainerTop;
+    }
+    if (itemBelow) {
+      itemBelowY = itemBelow.getBoundingClientRect().top - gridContainerTop;
+    }
+  }
 
-  return thisItem.animate(snap, snapTiming);
+  function animateSnap(thisItem, startPosition, endPosition, durationMS) {
+    const snap = [
+      { transform: `translate(0px, ${startPosition}px)` },
+      { transform: `translate(0px, ${endPosition}px)` },
+    ];
+    const snapTiming = {
+      duration: durationMS,
+    };
+
+    return thisItem.animate(snap, snapTiming);
+  }
 }
